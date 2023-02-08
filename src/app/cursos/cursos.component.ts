9ecb9cca99e959794e70305287e4208bce16bcf9
Renan Perin Ribeiro <renan.perinribeiro@outlook.com>
2023-02-08 18:00:39 -0300

    update

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { CursosService } from './cursos.service';
 
@@ -10,12 +12,30 @@ import { CursosService } from './cursos.service';
 export class CursosComponent {
 
   cursos!: any[];
+  pagina!: number;
+  inscricao!: Subscription
 
-  constructor(private cursosService: CursosService){
+  constructor(private cursosService: CursosService, private route: ActivatedRoute, private router: Router){
 
   }
 
   ngOnInit() {
     this.cursos = this.cursosService.getCursos()
+
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina']
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe()
+  }
+
+  proximaPagina() {
+    // this.pagina++
+    this.router.navigate(['/cursos'],
+    {queryParams: {'pagina': ++this.pagina}})
   }
 }
